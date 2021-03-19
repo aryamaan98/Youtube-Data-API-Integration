0211aeca8349b82a92cad57eaf359e9f66c90d53
aryamaan98 <aryamaan98@outlook.com>
2021-03-19 20:17:09 +0530

    created model videos details

--- youtubeDataApi/searchApi/models.py
+++ youtubeDataApi/searchApi/models.py
@@ -1,3 +1,16 @@
+import uuid
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class VideosDetail(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    youtube_video_id = models.TextField(max_length = 15,null=False, unique=True)
+    title = models.TextField(null=False)
+    description = models.TextField(null=True)
+    publish_datetime = models.DateTimeField(null=False)
+    thumbnail_url = models.URLField(max_length = 255, null=True)
+    created_at = models.DateTimeField(default=timezone.now) 
+
+    def __str__(self):
+        return str(self.id)
